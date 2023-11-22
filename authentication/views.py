c8b8d9b00fa3b1b1fe94126e25160a6f73ef711b
nandapratama21 <nanda241power@gmail.com>
2023-11-22 13:21:35 +0700

    FIX BUGS

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,11 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth import logout as auth_logout
+
 
 @csrf_exempt
 def login(request):
@@ -28,4 +32,55 @@ def login(request):
         return JsonResponse({
             "status": False,
             "message": "Login gagal, periksa kembali email atau kata sandi."
+        }, status=401)
+    
+
+
+
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logout berhasil!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout gagal."
+        }, status=401)
+    
+@csrf_exempt
+def register(request):
+
+    if request.method == "POST":
+        user_data = json.loads(request.body)
+        
+        username = user_data['username']
+        password1 = user_data['password1']
+        password2 = user_data['password2']
+
+        if password1 != password2:
+            return JsonResponse({
+            "status": False,
+            "message": "Register gagal, password confirmation failed"
+        }, status=401)
+
+        user = User.objects.create_user(username = username, password = password1)
+        user.save()
+
+        return JsonResponse({
+            "status": True,
+            "message": "Register sukses!"
+            # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
+        }, status=200)
+    
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Register gagal"
         }, status=401)
